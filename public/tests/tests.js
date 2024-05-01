39077b54ca2f4989bf70e593b3706eb1d6710a0c
oKaio122 <123851567+oKaio122@users.noreply.github.com>
2024-04-30 23:11:27 -0300

    first commit

---
+++ public/tests/tests.js
@@ -0,0 +1,41 @@
+// function randomizar(randomizarlista){
+//     let currentIndex = randomizarlista.length;
+//
+//     while (currentIndex !== 0) {
+//         let randomIndex = Math.floor(Math.random() * randomizarlista.length);
+//         currentIndex -= 1;
+//
+//         let temp = randomizarlista[currentIndex];
+//         randomizarlista[currentIndex] = randomizarlista[randomIndex];
+//         randomizarlista[randomIndex] = temp;
+//     }
+//     return randomizarlista
+// }
+// function gerarBaralhoRandom(){
+//     let naipes = ["C", "O", "P", "E"]
+//     let valores = ["4", "5", "6", "7", "D", "J", "K", "A", "2", "3"]
+//     let baralhoinicial = []
+//     for (let naipe of naipes){
+//         for (let valor of valores){
+//             baralhoinicial.push([naipe, valor])
+//         }
+//     }
+//     return randomizar(baralhoinicial)
+// }
+// baralho = gerarBaralhoRandom()
+//
+// cartamesa = baralho.pop()
+// console.log(cartamesa)
+// console.log(cartamesa[0])
+// console.log(cartamesa[1])
+// console.log(`${cartamesa[0]}_${cartamesa[1]}`)
+// const socket = io();
+//
+// function registrarJogadores(){
+//     jogadores = [346235252, "kaio", "time1"]
+// }
+// registrarJogadores()
+// console.log(jogadores)
+// export {registrarJogadores}
+
+
